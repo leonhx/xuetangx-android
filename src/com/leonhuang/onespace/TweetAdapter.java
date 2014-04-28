847766ca06957b34800e98d0f5c1ba0f8f1a460f
leon-huang <leon@njuopen.com>
2014-04-28 19:53:13 +0800

    Pull to Refresh

--- src/com/leonhuang/onespace/TweetAdapter.java
+++ src/com/leonhuang/onespace/TweetAdapter.java
@@ -1,21 +1,42 @@
 package com.leonhuang.onespace;
 
+import java.util.LinkedList;
+
 import android.app.Activity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-public class TweetAdapter extends ArrayAdapter {
+public class TweetAdapter extends ArrayAdapter<String> {
 	private LayoutInflater inflater;
+	private LinkedList<String> items;
 
-    public TweetAdapter(Activity activity, String[] items) {
+    public TweetAdapter(Activity activity, LinkedList<String> items) {
     	super(activity, R.layout.row_tweet, items);
         inflater = activity.getWindow().getLayoutInflater();
+        this.items = items;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-    	return inflater.inflate(R.layout.row_tweet, parent, false);
+    	View view = inflater.inflate(R.layout.row_tweet, parent, false);
+    	Log.i("0", view.toString());
+    	TextView text = (TextView) view.findViewById(R.id.tweetBrief); 
+    	Log.i("0", text.getText().toString());
+    	text.setText(items.get(position));
+    	return view;
+    }
+    
+    @Override
+    public int getCount() {
+    	return items.size();
+    }
+
+    @Override
+    public void notifyDataSetChanged() {
+    	super.notifyDataSetChanged();
     }
 }
--- src/com/leonhuang/onespace/TweetListActivity.java
+++ src/com/leonhuang/onespace/TweetListActivity.java
@@ -1,26 +1,80 @@
 package com.leonhuang.onespace;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+
 import android.app.ListActivity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.format.DateUtils;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnLastItemVisibleListener;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.State;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.handmark.pulltorefresh.library.extras.SoundPullEventListener;
+
 public class TweetListActivity extends ListActivity {
-    private ArrayAdapter<String> tweetItemArrayAdapter;
+    static final int MENU_MANUAL_REFRESH = 0;
+
+	private LinkedList<String> mListItems;
+	private PullToRefreshListView mPullRefreshListView;
+	private TweetAdapter mAdapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tweet_list);
 
-        tweetItemArrayAdapter = new TweetAdapter(this, new String[10]);
-        setListAdapter(tweetItemArrayAdapter);
+		mPullRefreshListView = (PullToRefreshListView) findViewById(R.id.pull_to_refresh_listview);
+
+		mPullRefreshListView.setOnRefreshListener(new OnRefreshListener<ListView>() {
+			@Override
+			public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+				String label = DateUtils.formatDateTime(getApplicationContext(), System.currentTimeMillis(),
+						DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_ABBREV_ALL);
+
+				refreshView.getLoadingLayoutProxy().setLastUpdatedLabel(label);
+
+				new GetDataTask().execute();
+			}
+		});
+
+		mPullRefreshListView.setOnLastItemVisibleListener(new OnLastItemVisibleListener() {
+
+			@Override
+			public void onLastItemVisible() {
+				Toast.makeText(TweetListActivity.this, getString(R.string.toast_end_of_list), Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		ListView actualListView = mPullRefreshListView.getRefreshableView();
+
+		registerForContextMenu(actualListView);
+
+		mListItems = new LinkedList<String>();
+		mListItems.addAll(Arrays.asList(mStrings));
+
+		mAdapter = new TweetAdapter(this, mListItems);
+
+		SoundPullEventListener<ListView> soundListener = new SoundPullEventListener<ListView>(this);
+		soundListener.addSoundEvent(State.PULL_TO_REFRESH, R.raw.pull_event);
+		soundListener.addSoundEvent(State.RESET, R.raw.reset_sound);
+		soundListener.addSoundEvent(State.REFRESHING, R.raw.refreshing_sound);
+		mPullRefreshListView.setOnPullEventListener(soundListener);
+
+		actualListView.setAdapter(mAdapter);
 	}
 	
 	@Override
@@ -33,17 +87,33 @@ public class TweetListActivity extends ListActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main, menu);
+		menu.add(0, MENU_MANUAL_REFRESH, 0, R.string.action_refresh);
         return super.onCreateOptionsMenu(menu);
     }
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_post:
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) menuInfo;
+
+		menu.setHeaderTitle("Item: " + getListView().getItemAtPosition(info.position));
+		menu.add("Item 1");
+		menu.add("Item 2");
+		menu.add("Item 3");
+		menu.add("Item 4");
+
+		super.onCreateContextMenu(menu, v, menuInfo);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+			case MENU_MANUAL_REFRESH:
+				new GetDataTask().execute();
+				mPullRefreshListView.setRefreshing(false);
+				break;
+			case R.id.action_post:
                 openPost();
-                return true;
-            case R.id.action_refresh:
-                refreshNow();
                 return true;
             case R.id.action_favorite:
             	openFavorite();
@@ -53,20 +123,17 @@ public class TweetListActivity extends ListActivity {
             	return true;
             default:
                 return super.onOptionsItemSelected(item);
-        }                
-    }
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
 
 	private void openPost() {
 		Intent intent = new Intent(this, PostActivity.class);
 	    startActivity(intent);
 	}
     
-    private void refreshNow() {
-		// TODO Auto-generated method stub
-    	Toast.makeText(this, "Refresh", Toast.LENGTH_SHORT).show();
-	}
-
-	private void openFavorite() {
+    private void openFavorite() {
 		// TODO Auto-generated method stub
 		Toast.makeText(this, "Favorite", Toast.LENGTH_SHORT).show();
 	}
@@ -75,5 +142,35 @@ public class TweetListActivity extends ListActivity {
 		// TODO Auto-generated method stub
 		Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
 	}
+	
+	private class GetDataTask extends AsyncTask<Void, Void, String[]> {
+
+		@Override
+		protected String[] doInBackground(Void... params) {
+			// Simulates a background job.
+			try {
+				Thread.sleep(4000);
+			} catch (InterruptedException e) {
+			}
+			return mStrings;
+		}
+
+		@Override
+		protected void onPostExecute(String[] result) {
+			mListItems.addFirst("Added after refresh...");
+			mAdapter.notifyDataSetChanged();
+
+			// Call onRefreshComplete when the list has been refreshed.
+			mPullRefreshListView.onRefreshComplete();
+
+			super.onPostExecute(result);
+		}
+	}
+	
+	private String[] mStrings = { "Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi",
+			"Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu", "Airag", "Airedale", "Aisy Cendre",
+			"Allgauer Emmentaler", "Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi",
+			"Acorn", "Adelost", "Affidelice au Chablis", "Afuega'l Pitu", "Airag", "Airedale", "Aisy Cendre",
+			"Allgauer Emmentaler" };
 
 }
