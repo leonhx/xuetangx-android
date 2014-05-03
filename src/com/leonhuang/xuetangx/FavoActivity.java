030062f9b874470ebe2fc6aae4b534d55fec95ce
leon-huang <leon@njuopen.com>
2014-05-03 16:57:04 +0800

    add XuetangXModel to dependency

--- src/com/leonhuang/xuetangx/FavoActivity.java
+++ src/com/leonhuang/xuetangx/FavoActivity.java
@@ -1,20 +1,12 @@
 package com.leonhuang.xuetangx;
 
-import java.io.IOException;
-
 import android.app.Activity;
-import android.graphics.PixelFormat;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.MediaController;
