9f2bd48514b2d8aa7706ae10b7942d8a19599a2f
leon-huang <leon@njuopen.com>
2014-05-04 20:00:44 +0800

    remove unused activities and corresponding layout and menu

--- src/com/leonhuang/xuetangx/CourseListActivity.java
+++ src/com/leonhuang/xuetangx/CourseListActivity.java
@@ -100,15 +100,16 @@ public class CourseListActivity extends ListActivity {
 
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
-		CurrentCourseItem course = mListItems.get(position - 1);
-		String url = mListItems.get(position - 1).getPath();
-		if (!course.isStarted()) {
-			url = "/courses/" + course.getUniversity() + "/" + course.getId()
-					+ "/_/about";
-		}
-		Intent intent = new Intent(this, CourseDetailActivity.class);
-		intent.putExtra(COURSE_URL, XuetangX.absPath(url));
-		startActivity(intent);
+		// TODO
+//		CurrentCourseItem course = mListItems.get(position - 1);
+//		String url = mListItems.get(position - 1).getPath();
+//		if (!course.isStarted()) {
+//			url = "/courses/" + course.getUniversity() + "/" + course.getId()
+//					+ "/_/about";
+//		}
+//		Intent intent = new Intent(this, CourseDetailActivity.class);
+//		intent.putExtra(COURSE_URL, XuetangX.absPath(url));
+//		startActivity(intent);
 	}
 
 	@Override
