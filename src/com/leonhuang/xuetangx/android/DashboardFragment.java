ad3822fabc18eb618c368fc5b9ae87641984223b
leon-huang <leon@njuopen.com>
2014-05-12 08:26:10 +0800

    almost complete

--- src/com/leonhuang/xuetangx/android/DashboardFragment.java
+++ src/com/leonhuang/xuetangx/android/DashboardFragment.java
@@ -28,41 +28,13 @@ public class DashboardFragment extends Fragment implements
 		mAppSectionsPagerAdapter = new AppSectionsPagerAdapter(getActivity()
 				.getSupportFragmentManager());
 
-		// Set up the action bar.
-		final ActionBar actionBar = getActivity().getActionBar();
-
-		// Specify that we will be displaying tabs in the action bar.
-		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
 		// Set up the ViewPager, attaching the adapter and setting up a listener
 		// for when the
 		// user swipes between sections.
 		mViewPager = (ViewPager) rootView.findViewById(R.id.pager);
 		mViewPager.setAdapter(mAppSectionsPagerAdapter);
-		mViewPager
-				.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-					@Override
-					public void onPageSelected(int position) {
-						// When swiping between different app sections, select
-						// the corresponding tab.
-						// We can also use ActionBar.Tab#select() to do this if
-						// we have a reference to the
-						// Tab.
-						actionBar.setSelectedNavigationItem(position);
-					}
-				});
-
-		// For each of the sections in the app, add a tab to the action bar.
-		for (int i = 0; i < mAppSectionsPagerAdapter.getCount(); i++) {
-			// Create a tab with text corresponding to the page title defined by
-			// the adapter.
-			// Also specify this Activity object, which implements the
-			// TabListener interface, as the
-			// listener for when this tab is selected.
-			actionBar.addTab(actionBar.newTab()
-					.setText(mAppSectionsPagerAdapter.getPageTitle(i))
-					.setTabListener(this));
-		}
+		
+		mViewPager.setCurrentItem(1);
 
 		return rootView;
 	}
