25ece5026525fd9b7c575572a7ade61cd19b6167
leon-huang <leon@njuopen.com>
2014-05-14 15:38:04 +0800

    more course detail

--- src/com/leonhuang/xuetangx/android/ItemAdapter.java
+++ src/com/leonhuang/xuetangx/android/ItemAdapter.java
@@ -5,12 +5,16 @@ import java.util.ArrayList;
 import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.leonhuang.xuetangx.R;
 import com.leonhuang.xuetangx.data.ItemInfo;
+import com.leonhuang.xuetangx.data.ItemType;
 
 public class ItemAdapter extends ArrayAdapter<ItemInfo> {
 	private LayoutInflater inflater;
@@ -24,10 +28,32 @@ public class ItemAdapter extends ArrayAdapter<ItemInfo> {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		View view = inflater.inflate(R.layout.row_lecture, parent, false);
-		TextView title = (TextView) view.findViewById(R.id.lecture_title);
+		View view = inflater.inflate(R.layout.row_item, parent, false);
+		TextView title = (TextView) view.findViewById(R.id.item_title);
+		ImageButton download = (ImageButton) view
+				.findViewById(R.id.item_download_image_button);
+		ImageView type = (ImageView) view.findViewById(R.id.item_type_image);
 
-		title.setText(items.get(position).getTitle());
+		ItemInfo item = items.get(position);
+
+		if (item.getType() == ItemType.PROBLEM) {
+			type.setImageDrawable(view.getResources().getDrawable(
+					R.drawable.ic_action_edit));
+			download.setVisibility(ImageButton.INVISIBLE);
+		} else {
+			download.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View arg0) {
+					// TODO
+
+				}
+			});
+			download.setVisibility(ImageButton.INVISIBLE); // TODO remove after
+															// download OK
+		}
+
+		title.setText(item.getTitle());
 
 		return view;
 	}
